459d40744b36ecb7addc4c82ee8c4fc544cf93be
Colin <colinheng@gmail.com>
2013-07-06 10:48:46 +0000

    updated again

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var responsestring = new Buffer(27);
 
-fs.readFile(indexhtml, function (err, responsestring)) {
+fs.readFile(indexhtml, function (err, responsestring) {
   if (err) throw err;
   console.log(buf(toString(responsestring));
 });
@@ -16,3 +16,13 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
+
+var responsestring = new Buffer(27);
+fs.readFile(indexhtml, function (err, responsestring)) {
+  if (err) throw err;
+  console.log(buf(toString(responsestring));
+});
+
