8801e3b530febc6abd0565ab9d3702576208bf1b
Colin <colinheng@gmail.com>
2013-07-06 10:56:36 +0000

    ok

--- web.js
+++ web.js
@@ -1,19 +1,19 @@
 var indexhtml = "index.html";
 var express = require('express');
+var fs = require('fs');
 var app = express.createServer(express.logger());
 var responsestring = new Buffer(27);
 
 fs.readFile(indexhtml, function (err, responsestring) {
   if (err) throw err;
-  console.log(buf(toString(responsestring)));
+  console.log(responsestring.toString('utf-8'));
 });
 
 app.get('/', function(request, response) {
-  response.send(responsestring);
+  response.send(responsestring.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
